fae621f7c61efcd067d8935fac6a52f87d12bd1e
Harry <changyuhao625@gmail.com>
2018-06-20 18:57:56 +0800

    Fix Bug
    Fix Bug

---
+++ Dto/StockQueryDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using KendoGridBinder;
+
+namespace Redoak.Domain.Model.Dto
+{
+    public class StockQueryDto : KendoGridBaseRequest
+    {
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+
+        public string Name { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int Id { get; set; }
+    }
+}
